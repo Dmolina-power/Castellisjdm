0f7ae894b34e7ff50b9bc0516dd5e9d27215bdd7
daniel molina <bigdpower@icloud.com>
2021-03-23 23:40:06 -0700

    added company lgo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './castellisjdm.jpg';
 import './App.css';
 
 function App() {
@@ -6,18 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          danny
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
       </header>
     </div>
   );
